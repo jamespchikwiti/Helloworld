31e4de44e386c696cb896a0e23b9b2834367b093
jamespchikwiti <66641093+jamespchikwiti@users.noreply.github.com>
2020-06-08 22:04:39 +0200

    Create Public class Exchange.java

---
+++ Public class Exchange.java
@@ -0,0 +1,12 @@
+public class exchange {
+  publick static void main(string[]. args)
+  {
+    int a = Iteger.parseInt(args[0]);
+    int b = Iteger.parseInt(args[1]);
+    int t = a;
+    a =b;
+    b =t;
+    System.out.println(a);
+    System.out.println(b)
+    }
+}
